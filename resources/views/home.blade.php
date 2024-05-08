8458a39b9c2f77d8d649cf4fc9373d2b2249dcac
EmilianaManzo <emiliana.ma96@gmail.com>
2024-05-08 15:29:13 +0200

    scaffolding e pagina home con route

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{-- Bootstrap --}}
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css' integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==' crossorigin='anonymous'/>
+
+    <title>Document</title>
+</head>
+<body>
+    <div class="container d-flex justify-content-center align-items-center mt-5 ">
+        <h1 class=" text-danger ">Hello World</h1>
+    </div>
+</body>
+</html>
